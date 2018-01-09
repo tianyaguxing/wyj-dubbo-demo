8bba8c596bb9671792fe34c7f0f694ff2a11f52b
wangyuanjun008 <1098345452@qq.com>
2018-01-09 16:18:50 +0800

    create dubbo-demo project

---
+++ wyj-dubbo-api/src/main/java/com/wyj/dubbo/DemoService.java
@@ -0,0 +1,13 @@
+package com.wyj.dubbo;
+/**
+ * 定义服务接口
+ * 
+ * 
+ * @author：WangYuanJun
+ * @date：2018年1月9日 下午4:03:40
+ */
+public interface DemoService {
+    
+    String sayHello(String name);
+    
+}
